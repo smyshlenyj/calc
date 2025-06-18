77db0ceff42ee0c28e7fc71bc8010413d2b47f0d
j <smyshlenyj@gmail.com>
2025-06-18 23:05:49 +0300

    * minor fixes

--- main.cpp
+++ main.cpp
@@ -5,57 +5,51 @@ int main() {
   std::cout << "Калькулятор";
 
   while (true) {
-    std::cout << "\nВведите: \n+ для сложения \n- для вычитания \n* для "
-                 "умножения \n/ для деления \n^ для возведения в степень\nf "
-                 "для вычисления факториала числа\n\nи q для выхода из программы"
-              << std::endl;
+    std::cout
+        << "\nВведите: \n+ для сложения \n- для вычитания \n* для "
+           "умножения \n/ для деления \n^ для возведения в степень\nf "
+           "для вычисления факториала числа\n\nи q для выхода из программы"
+        << std::endl;
     char choice;
     std::cin >> choice;
     if (choice == 'q') {
       std::cout << "Всего доброго!" << std::endl;
-      ;
       return 0;
     }
-
     int first = 0;
     int second = 0;
-    if (choice != 'f') {
+    if (choice == 'f') {
+      std::cout << "Введите число" << std::endl;
+      std::cin >> first;
+    } else if (choice == '+' || choice == '-' || choice == '*'|| choice == '/' || choice == '^') {
       std::cout << "Введите: первое число" << std::endl;
       std::cin >> first;
       std::cout << "Введите: второе число" << std::endl;
       std::cin >> second;
     }
 
-    if (choice == 'f') {
-      std::cout << "Введите число" << std::endl;
-      std::cin >> first;
-    }
-
     switch (choice) {
-    case '+': {
+    case '+':
       std::cout << math::add(first, second) << std::endl;
       break;
-    }
-    case '-': {
+    case '-':
       std::cout << math::subtract(first, second) << std::endl;
       break;
-    }
-    case '*': {
+    case '*':
       std::cout << math::multiply(first, second) << std::endl;
       break;
-    }
-    case '/': {
+    case '/':
       std::cout << math::divide(first, second) << std::endl;
       break;
-    }
-    case '^': {
+    case '^':
       std::cout << math::pow(first, second) << std::endl;
       break;
-    }
-    case 'f': {
+    case 'f':
       std::cout << math::getFactorial(first) << std::endl;
       break;
-    }
+    default:
+      std::cout << "Неизвестная операция. Повторите ввод." << std::endl;
+      break;
     }
   }
   return 0;
